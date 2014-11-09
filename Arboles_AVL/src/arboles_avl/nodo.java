e4036cfc27270c01353105746222fe9297de5102
jacero94 <JAVIER@JAVIER-PC>
2014-11-09 13:10:10 -0500

    Creación del proyecto de arboles AVL insertando clase nodo y arboles

---
+++ Arboles_AVL/src/arboles_avl/nodo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arboles_avl;
+
+/**
+ *
+ * @author JAVIER
+ */
+public class nodo {
+    int dato;
+    int fb;
+    nodo izq, der;
+
+    public nodo(int n) {
+        this.dato = n;
+        this.fb = 0;
+        izq = der = null;
+
+    }
+
+    
+}
